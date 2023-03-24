bac37104292af20ac89b57b6d849783737994718
tmdt.hk211 <tmdt.hk211@gmail.com>
2023-03-24 15:10:47 +0700

    [fix] align utils

--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,7 @@ from typing import Tuple
 import pygame
 
 def align(x: int, y: int, width: int, height: int, anchor: Align) -> Tuple[int, int]:
-    return x - width / 2  * anchor.value[0], y - height / 2 * anchor.value[1]
+    return x - width / 2  * anchor.value[1], y - height / 2 * anchor.value[0]
 
 def scale(img: pygame.Surface, factor: float) -> pygame.Surface:
     w, h = img.get_width() * factor, img.get_height() * factor
